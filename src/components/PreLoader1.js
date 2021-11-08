e4ce3fb3927f5a4a2110076fdfae7c2e855a6b87
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2021-11-08 11:24:56 -0800

    Initial upload
    Front end for NFT Contract

---
+++ src/components/PreLoader1.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
+
+function PreLoader1() {
+  const [data, setData] = useState([]);
+  const [done, setDone] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setDone(true);
+        });
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+        <div className="loading">
+      {!done ? (
+        <ReactLoading
+          type={"bars"}
+          color={"#03fc4e"}
+          height={100}
+          width={100}
+        />
+      ) : (
+        <p>
+          Minting Complete
+        </p>
+      )}
+        </div>
+
+    </>
+  );
+}
+
+export default PreLoader1;
