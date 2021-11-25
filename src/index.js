c6868c670f42bb7dc1a3b640a4b3905108c1fc9b
saunic <saunic8@gmail.com>
2021-11-25 15:03:50 -0700

    Create a new App file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, theme } from "./styles/theme";
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
-import Appp from "./Appp";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Appp />
-      <Footer />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
