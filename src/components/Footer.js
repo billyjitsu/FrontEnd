084245dd666b83ac786a200014c075760e509689
saunic <saunic8@gmail.com>
2021-11-26 11:20:47 -0700

    Remove css as it is unused

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import Logo from "../assets/1hiveFullLogo.svg";
 import { navLinks } from "../data/navLinks";
 
